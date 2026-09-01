b36da52e379f5112015bd3ec73f3e76e15d1f901
MuriloDamaceno <damacenomurilo01@gmail.com>
2026-08-31 21:03:05 -0300

    Adiciona middleware de autorizacao por papel (organizador)

---
+++ middlewares/organizadorMiddleware.js
@@ -0,0 +1,12 @@
+/**
+ * Bloqueia o acesso a rotas exclusivas de organizador.
+ * Deve ser usado sempre APÓS o authMiddleware, já que depende de req.session.user existir.
+ */
+function organizadorMiddleware(req, res, next) {
+  if (req.session.user && req.session.user.tipo === 'organizador') {
+    return next();
+  }
+  return res.status(403).send('Acesso negado: apenas organizadores podem realizar esta ação.');
+}
+
+module.exports = organizadorMiddleware;
--- routes/eventoRoutes.js
+++ routes/eventoRoutes.js
@@ -2,15 +2,16 @@ const express = require('express');
 const router = express.Router();
 const eventoController = require('../controllers/eventoController');
 const inscricaoController = require('../controllers/inscricaoController');
+const organizadorMiddleware = require('../middlewares/organizadorMiddleware');
 
 router.get('/', eventoController.listar);
-router.get('/novo', eventoController.telaCriar);
-router.post('/novo', eventoController.criar);
+router.get('/novo', organizadorMiddleware, eventoController.telaCriar);
+router.post('/novo', organizadorMiddleware, eventoController.criar);
 
 router.get('/:id', eventoController.detalhes);
-router.get('/:id/editar', eventoController.telaEditar);
-router.post('/:id/editar', eventoController.atualizar);
-router.post('/:id/deletar', eventoController.deletar);
+router.get('/:id/editar', organizadorMiddleware, eventoController.telaEditar);
+router.post('/:id/editar', organizadorMiddleware, eventoController.atualizar);
+router.post('/:id/deletar', organizadorMiddleware, eventoController.deletar);
 
 router.post('/:id/inscrever', inscricaoController.inscrever);
 
